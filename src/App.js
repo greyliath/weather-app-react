66c1580218c11de090885d316f79535778468291
greyliath <kbroussardco@gmail.com>
2022-04-13 13:16:54 +0100

    adds link to github repo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        <a href="https://github.com/greyliath/weather-app-react" target="_blank">open-sourced code by greyliath</a>
+      </footer>
     </div>
   );
 }
