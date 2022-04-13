8d405eb4c577fe62e114ca6d4381e59b2b35953d
greyliath <kbroussardco@gmail.com>
2022-04-13 13:38:13 +0100

    basic html skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div class="container">
-      Weather App
+      <Weather />
       <footer>
         <a href="https://github.com/greyliath/weather-app-react" target="_blank" rel="noreferrer">open-sourced code by greyliath</a>
       </footer>
