c1fda3afc5de6857610ed88d461f73cb6bbca145
greyliath <kbroussardco@gmail.com>
2022-04-13 13:23:09 +0100

    Adds bootstrap

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div class="container">
       Weather App
       <footer>
-        <a href="https://github.com/greyliath/weather-app-react" target="_blank">open-sourced code by greyliath</a>
+        <a href="https://github.com/greyliath/weather-app-react" target="_blank" rel="noreferrer">open-sourced code by greyliath</a>
       </footer>
+    </div>
     </div>
   );
 }
