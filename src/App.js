37c63e85315a93b5bcc91997b4cd234d87ea9494
greyliath <kbroussardco@gmail.com>
2022-04-14 13:45:44 +0100

    Fixes more netlify errors

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
           <a
             href='https://github.com/greyliath/weather-app-react'
             target='_blank'
-            rel='noreferrer'
+            rel='noopener noreferrer'
           >
             open-sourced code</a> by greyliath
         </footer>
